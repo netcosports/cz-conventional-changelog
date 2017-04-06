bcf01442377a93b67b32f387111ba67e9975251e
zmarouf <zeid.marouf@gmail.com>
2017-04-06 16:40:12 +0200

    feat(types): Add bahmutov/next-ver compatible types

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 var engine = require('./engine');
 var conventionalCommitTypes = require('conventional-commit-types');
 
+conventionalCommitTypes.types.minor = { description: 'A feature (alias feat)' };
+conventionalCommitTypes.types.break = { description: 'A breaking change' };
+conventionalCommitTypes.types.patch = { description: 'A patch (alias fix)' };
+conventionalCommitTypes.types.major = { description: 'A major non-backward compatible feature (alias break)' };
+
 module.exports = engine({
   types: conventionalCommitTypes.types
 });
